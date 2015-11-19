29a503311b7ae60c7d8212b986061d59e054eefa
KierstonHill83 <khill1218@gmail.com>
2015-11-18 23:30:35 -0700

    Started mocha tests

---
+++ server/dropActive.sql
@@ -0,0 +1,5 @@
+drop TABLE friends cascade;
+drop TABLE userChatRooms cascade;
+drop TABLE activityProperty cascade;
+drop TABLE userActivity cascade;
+drop TABLE userInfo cascade;
--- server/test/test-server.js
+++ server/test/test-server.js
@@ -1,28 +1,27 @@
 var fs = require('fs');
 var path  = require('path');
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/user';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/activeData';
 var client = new pg.Client(connectionString);
 
 
 describe('database', function() {
   client.connect();
   // Drop the database
-  var query = client.query('DROP DATABASE activeData');
-  // Create the database
-  var query = client.query('CREATE DATABASE activeData');
+  var q = fs.readFileSync(path.join(__dirname, '../dropActive.sql'), 'utf8');
+  client.query(q);
   // Create all the tables in the database
   var q = fs.readFileSync(path.join(__dirname, '../active.sql'), 'utf8');
   client.query(q);
 
-  beforeEach(function(done) {
-    client.query('INSERT INTO userInfo (username, password, name, email, age, gender, location) VALUES("kierston", "test", "tester", "test@gmail.com", 20, "female", "denver")');
+  beforeEach(function() {
+    client.query('INSERT INTO userInfo (username, password, name, email, age, gender, location) VALUES(\'kierston\', \'test\', \'tester\', \'test@gmail.com\', 20, \'female\', \'denver\')');
   });
 
   it('should list all users', function(done) {
     client.query('SELECT * FROM userInfo', function(err, res){
-        res.should.be.a('array');
-        res[0].should.have.property('userId');
+        // res.should.be.a('array');
+        res.rows[0].should.have.property('userId');
         res[0].should.have.property('username');
         res[0].should.have.property('password');
         res[0].should.have.property('name');
@@ -33,7 +32,6 @@ describe('database', function() {
         res[0].should.have.property('image');
         res[0].userId.should.equal('1');
         res[0].username.should.equal('kierston');
-        done();
     });
   });
 
